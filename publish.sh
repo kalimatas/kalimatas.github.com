a41a3918f98530758b70e0d45de38bd178f593f6
Alexander Guz <kalimatas@gmail.com>
2021-08-20 11:27:31 +0200

    Build site on current branch

--- publish.sh
+++ publish.sh
@@ -7,8 +7,9 @@ COMMIT_MESSAGE="${1:-"Published on "`date +%Y-%m-%d`}"
 current_branch=`git branch --show-current`
 gh_pages_branch="gh-pages"
 
-git checkout $gh_pages_branch
 bundle exec jekyll build
+
+git checkout $gh_pages_branch
 cp -r _site/ .
 git commit -am "${COMMIT_MESSAGE}"
 git push origin $gh_pages_branch
