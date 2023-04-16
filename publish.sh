8e96f7d1220c418a30c1ed6240b0f858b3747977
Alexander Guz <alexander.guz@flixbus.com>
2023-04-16 14:56:14 +0200

    change publish

--- publish.sh
+++ publish.sh
@@ -11,6 +11,7 @@ bundle exec jekyll build
 
 git checkout $gh_pages_branch
 cp -r _site/ .
+git add .
 git commit -am "${COMMIT_MESSAGE}"
 git push origin $gh_pages_branch
 git checkout $current_branch
